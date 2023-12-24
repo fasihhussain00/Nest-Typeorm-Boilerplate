5b84f99f03f20ac3128e6392990c19007b543329
Fasih Hussain <fasihhussain00@gmail.com>
2023-12-25 00:27:49 +0500

    chore: liveness and readiness probes added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './db/config';
+import { HealthModule } from './health/health.module';
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({ cache: true, ignoreEnvFile: true }),
     TypeOrmModule.forRoot(dataSourceOptions),
+    HealthModule,
   ],
 })
 export class AppModule {}
---
+++ src/health/health.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheckService,
+  HealthCheck,
+  TypeOrmHealthIndicator,
+  HealthCheckResult,
+  HealthIndicatorResult,
+} from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+  ) {}
+  @Get('liveness')
+  @HealthCheck()
+  async check(): Promise<HealthCheckResult> {
+    return this.health.check([
+      async (): Promise<HealthIndicatorResult> => ({
+        api: { status: 'up' },
+      }),
+    ]);
+  }
+  @Get('readiness')
+  @HealthCheck()
+  async checkReadiness(): Promise<HealthCheckResult> {
+    return this.health.check([
+      async (): Promise<HealthIndicatorResult> => this.db.pingCheck('postgres'),
+    ]);
+  }
+}
